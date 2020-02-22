41b390c101d727284a3c540992193b1584376668
Kirill Arkhypov <k.arkhypov@gmail.com>
2020-02-22 15:22:53 +0200

    implemented basic functionality

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,32 @@
-console.log('hello');
+const inputForm = document.querySelector('.input-form');
+const taskList = document.querySelector('.list');
+
+function addTask(task) {
+	const li = document.createElement('li');
+	li.classList.add('list__item');
+	li.innerHTML = `<p class="list__text">${task}</p>
+									<i class="list__remove fas fa-trash-alt"></i>`;
+	taskList.append(li);
+}
+
+inputForm.addEventListener('submit', e => {
+	e.preventDefault();
+	const newTask = e.currentTarget.inputForm.value;
+	if (!newTask) return;
+	addTask(newTask);
+	e.target.reset();
+});
+
+taskList.addEventListener('click', e => {
+	if (e.target.matches('.list__remove')) {
+		const listItem = e.target.closest('.list__item');
+		listItem.classList.toggle('fade');
+		setTimeout(() => {
+			listItem.remove();
+		}, 500);
+	}
+
+	if (e.target.matches('.list__text')) {
+		e.target.classList.toggle('checked');
+	}
+});
