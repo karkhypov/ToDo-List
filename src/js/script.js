708bdb27f8d25a2f1edcada4387c423d25380369
Kirill Arkhypov <k.arkhypov@gmail.com>
2020-02-27 23:03:44 +0200

    added eslint & prettier, basic configuration

--- src/js/script.js
+++ src/js/script.js
@@ -3,32 +3,32 @@ const taskList = document.querySelector('.list');
 const newTaskTemplate = document.querySelector('#newTaskTemplate');
 
 function addTask(task) {
-	const newTask = document.importNode(newTaskTemplate.content, true);
-	newTask.querySelector('.list__text').textContent = task;
-	taskList.prepend(newTask);
-	setTimeout(() => {
-		document.querySelector('.list__item').classList.remove('fade');
-	}, 0);
+  const newTask = document.importNode(newTaskTemplate.content, true);
+  newTask.querySelector('.list__text').textContent = task;
+  taskList.prepend(newTask);
+  setTimeout(() => {
+    document.querySelector('.list__item').classList.remove('fade');
+  }, 0);
 }
 
 inputForm.addEventListener('submit', e => {
-	e.preventDefault();
-	const userInput = e.currentTarget.inputForm.value;
-	if (!userInput) return;
-	addTask(userInput);
-	e.target.reset();
+  e.preventDefault();
+  const userInput = e.currentTarget.inputForm.value;
+  if (!userInput) return;
+  addTask(userInput);
+  e.target.reset();
 });
 
 taskList.addEventListener('click', e => {
-	if (e.target.matches('.list__remove-icon')) {
-		const listItem = e.target.closest('.list__item');
-		listItem.classList.add('fade');
-		setTimeout(() => {
-			listItem.remove();
-		}, 350);
-	}
+  if (e.target.matches('.list__remove-icon')) {
+    const listItem = e.target.closest('.list__item');
+    listItem.classList.add('fade');
+    setTimeout(() => {
+      listItem.remove();
+    }, 350);
+  }
 
-	if (e.target.matches('.list__text')) {
-		e.target.classList.toggle('checked');
-	}
+  if (e.target.matches('.list__text')) {
+    e.target.classList.toggle('checked');
+  }
 });
