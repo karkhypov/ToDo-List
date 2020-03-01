459de22692567d1334e4039f73c12c0acc9090c3
Kirill Arkhypov <k.arkhypov@gmail.com>
2020-03-01 22:44:00 +0200

    implemented basic localStorage logic, refactoring required

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,44 @@
 const inputForm = document.querySelector('.input-form');
 const taskList = document.querySelector('.list');
-const newTaskTemplate = document.querySelector('#newTaskTemplate');
 
-function addTask(task) {
-  const newTask = document.importNode(newTaskTemplate.content, true);
-  newTask.querySelector('.list__text').textContent = task;
-  taskList.prepend(newTask);
+const tasks = [];
+
+const randomID = () => {
+  return Math.random()
+    .toString(36)
+    .slice(2, 12);
+};
+
+function updateStorage() {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function addTask(task, checked = false) {
+  const newTaskItem = {
+    value: task,
+    ID: randomID(),
+    checked,
+  };
+  const li = document.createElement('li');
+  li.classList.add('list__item', 'fade');
+  li.id = newTaskItem.ID;
+  li.innerHTML = `<p class="list__text"></p>
+	<i class="list__remove fas fa-trash-alt"></i>`;
+  taskList.prepend(li);
+  document.querySelector('.list__text').textContent = task;
+  tasks.push(newTaskItem);
   setTimeout(() => {
-    document.querySelector('.list__item').classList.remove('fade');
+    li.classList.remove('fade');
   }, 0);
+  updateStorage();
+}
+
+function loadFromStorage() {
+  const storageItems = JSON.parse(localStorage.getItem('tasks'));
+  localStorage.clear();
+  if (storageItems.length) {
+    storageItems.forEach(task => addTask(task.value, task.checked));
+  }
 }
 
 inputForm.addEventListener('submit', e => {
@@ -20,15 +50,23 @@ inputForm.addEventListener('submit', e => {
 });
 
 taskList.addEventListener('click', e => {
+  const listItem = e.target.closest('.list__item');
+  const taskInStorage = tasks.find(i => i.ID === listItem.id);
+
   if (e.target.matches('.list__remove')) {
-    const listItem = e.target.closest('.list__item');
     listItem.classList.add('fade');
     setTimeout(() => {
+      tasks.splice(tasks.indexOf(taskInStorage), 1);
       listItem.remove();
+      updateStorage();
     }, 350);
   }
 
   if (e.target.matches('.list__text')) {
     e.target.classList.toggle('checked');
+    taskInStorage.checked = !taskInStorage.checked;
+    updateStorage();
   }
 });
+
+document.addEventListener('DOMContentLoaded', loadFromStorage);
